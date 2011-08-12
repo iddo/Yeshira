6fa07dd184d9f2e253f850279ebd2a79a16ce531
iddo <iddo@iddo.co.il>
2011-08-12 12:09:33 +0300

    Fix dev setup script

--- yeshira/web/dev-setup.sh
+++ yeshira/web/dev-setup.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 # This script takes care of most of the steps for setting up a working development environment
-# The script assumes debian based linux in 64bit architecture. If you have a different system
+# The script assumes debian based linux in 64bit architecture. IT WILL NOT WORK ON ANY DIFFERENT CONFIGURATION.
+# Please change the relevant commands to make them work on other system configurations.
 #
 # Note: this script hasn't been tested yet, it is recommended you run it line by line to check that it does what it says
 
@@ -32,17 +33,16 @@ echo
 echo "*********** Development environment installed in $YESHIRA_HOME"
 echo	
 echo "*********** To run a local jetty server use this command"	
-echo $YESHIRA_HOME/workspace/Yeshira/yeshira/web/start-server.sh"
+echo "$YESHIRA_HOME/workspace/Yeshira/yeshira/web/start-server.sh"
 echo
-# TODO: make this work
 #echo "*********** Installing Eclipse plugins - Maven, Git"
 #$YESHIRA_HOME/eclipse/eclipse -application org.eclipse.equinox.p2.director -repository "http://download.eclipse.org/egit/updates" -installIU "org.eclipse.egit,org.eclipse.jgit"
-echo "*********** To finish configuration do the following steps:
-echo "1. Install 'Maven Integration for Eclipse' and 'EGit' Eclipse plugins through the Eclipse Market Place
-echo "2. Setup Maven plugin to support project settings (Window->Preferences->Maven->User Settings, and choose user settings file settings.xml located in $YESHIRA_HOME/workspace/Yeshira/yeshira/web), and to use the maven installation at $YESHIRA_HOME/apache-maven-3.0.3 
+echo "*********** To finish configuration do the following steps:"
+echo "1. Install 'Maven Integration for Eclipse' and 'EGit' Eclipse plugins through the Eclipse Market Place"
+echo "2. Setup Maven plugin to support project settings (Window->Preferences->Maven->User Settings, and choose user settings file settings.xml located in $YESHIRA_HOME/workspace/Yeshira/yeshira/web), and to use the maven installation at $YESHIRA_HOME/apache-maven-3.0.3"
 echo "3. Import yeshira web project from $YESHIRA_HOME/workspace/Yeshira (File->Import->Existing project into workspace)"
-echo "4. Right click on project and select Team->Share Project->Git->Use or Create repository in parent folder of project
-echo "5. Right click on project and select 
+echo "4. Right click on project and select Team->Share Project->Git->Use or Create repository in parent folder of project"
+echo "5. Right click on project and select"
 echo
 echo "*********** Starting Eclipse..."
 
